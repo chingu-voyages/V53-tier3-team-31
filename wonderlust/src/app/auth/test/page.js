6a9e6599abbee0ab5f08e5631ddd60ef0b8eec08
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-01-26 19:28:50 +0530

    test workflow pr

--- wonderlust/src/app/auth/test/page.js
+++ wonderlust/src/app/auth/test/page.js
@@ -1,1 +1,9 @@
 // to test workflow works 
+export default function SignUp() {
+    return (
+        <div className="form-container bg-gray-400">
+         
+              Welcome to Wanderlust
+              </div>
+              )
+}
