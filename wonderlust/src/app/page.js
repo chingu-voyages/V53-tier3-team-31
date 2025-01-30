f46e2592a881708582b86600bc0f29374cb173b9
umesh <umesharma.us11@gmail.com>
2025-01-30 11:58:43 +0530

    Implemented the UI for create trip form, added budget field for user to set the budget

--- wonderlust/src/app/page.js
+++ wonderlust/src/app/page.js
@@ -1,12 +1,5 @@
-import CreateTripForm from '../components/CreateTripForm';
-
 export default function Home() {
   return (
-    <div className="flex justify-center align-center bg-black/50">
-      Home
-      <div className="">
-        <CreateTripForm />
-      </div>
-    </div>
+    <div className="flex justify-center align-center bg-black/50">Home</div>
   );
 }
