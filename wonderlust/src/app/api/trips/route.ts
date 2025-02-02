d5154c92528dfcbc6e1f6dece624aec609b820b5
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2025-02-02 20:42:15 +0530

    CRUD for trips Api complete (#60)
    * CRUD for trips Api complete

--- wonderlust/src/app/api/trips/route.ts
+++ wonderlust/src/app/api/trips/route.ts
@@ -2,38 +2,33 @@ import {NextRequest, NextResponse} from "next/server";
 import Trip from '@/models/CreateTrip'
 import { HttpStatusCode } from "axios";
 import connectMongo from "@/util/connect-mongo";
+import mongoose from "mongoose";
+
+// Get somehow the user objectID here using middleware
+const tempUserID = "6792633eaa08eb6efef02261" 
 
 
-
-export async function POST(req: NextRequest) { 
- 
- try {
-  await connectMongo();
-  const body = await req.json()
-
-  const { tripname, budget, travellers,
-   startDay, endDay,
-   destination ,userId} = body;
-  const parsedStartDay = new Date(startDay);
-  const parsedEndDay = new Date(endDay);
-  
-  const newTrip = new Trip({
-   tripname:tripname,
-   budget:budget,
-   travellers:travellers,
-   destination: destination,
-   startDay:parsedStartDay,
-   endDay: parsedEndDay,
-   user:userId
-  });
-
-   await Trip.create(newTrip)
-  
-     return NextResponse.json({ success: true, newTrip }, { status: HttpStatusCode.Created, });
-     
- } catch (error) {
-  return NextResponse.json(   { status: HttpStatusCode.BadRequest,message:"not working, error on the server"}, )
+export async function GET(req: NextRequest) {
+  try {
+    await connectMongo();
+    const tripList = await Trip.find({ user: new mongoose.Types.ObjectId(tempUserID) });
+    console.log(tripList)
+    if (tripList.length > 0){
+    return NextResponse.json({
+       success: true,
+       tripList : tripList
+    })
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "No trip found"
+     })
+    }
+}catch (error) {
+  console.error("GET Error:", error);
+  return NextResponse.json(
+    { success: false, message: "Internal Server Error" },
+    { status: HttpStatusCode.InternalServerError }
+  );
 }
 }
-
-
