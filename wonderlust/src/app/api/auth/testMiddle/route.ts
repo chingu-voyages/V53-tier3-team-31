2c56c925f0c219266c1c256b7495b1e3333e0730
Zarrar Hussain Abid <zarrarabid72@gmail.com>
2025-02-03 09:48:15 +0500

    fix middleware

--- wonderlust/src/app/api/auth/testMiddle/route.ts
+++ wonderlust/src/app/api/auth/testMiddle/route.ts
@@ -3,19 +3,16 @@ import { authenticateJWT } from '../../../../../util/middleware';
 import User from '../../../../../models/User';
 
 export async function GET(req) {
-    // console.log("asdasdasd")
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         authenticateJWT(req, {
             json: (data) => resolve(NextResponse.json(data)),
-            status: (statusCode) => ({
-                json: (data) => resolve(NextResponse.json(data, { status: statusCode })),
-            }),
+            status: (statusCode) => resolve(NextResponse.json({ message: 'Error occurred' }, { status: statusCode })),
         }, async () => {
             try {
                 const users = await User.find({});
                 resolve(NextResponse.json(users));
             } catch (error) {
-                reject(NextResponse.json({ message: 'Could not fetch users' }, { status: 500 }));
+                resolve(NextResponse.json({ message: 'Could not fetch users' }, { status: 500 }));
             }
         });
     });
