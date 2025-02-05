9f160650e1f03e56b4da33ec83222b717029941e
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2025-02-05 21:44:57 +0530

    POST , Put and delete integration (#63)

--- wonderlust/src/app/dashboard/page.js
+++ wonderlust/src/app/dashboard/page.js
@@ -11,22 +11,34 @@ import DeleteConfirm from "@/src/components/DeleteConfirm";
 export default function Dashboard() {
   const [trips, setTrips] = useState([
     {
-      title: "Sample Card 1",
-      date: "Aug 1 - Aug 15, 2024",
-      budget: "$5,000",
-      travelers: 3,
+      tripname: "Beach Getaway",
+      destination: "Delhi",
+      budget: "2000",
+      travellers: "3",
+      startDay: "2025-07-01T00:00:00.000Z",
+      endDay: "2025-07-10T00:00:00.000Z",
+      user: "6792633eaa08eb6efef02261",
+      tripObjId: "679e6e05189dd60ff447e7fd"
     },
-    {
-      title: "Sample Card 2",
-      date: "Nov 10 - Nov 30, 2024",
-      budget: "$3,000",
-      travelers: 2,
+    { 
+      tripname: "Sample Card 2",
+      destination: "Delhi",
+      budget: "2000",
+      travellers: "3",
+      startDay: "2025-07-01T00:00:00.000Z",
+      endDay: "2025-07-10T00:00:00.000Z",
+      user: "6792633eaa08eb6efef02261",
+      tripObjId: "679e6e05189dd60ff447e7fd"
     },
-    {
-      title: "Sample Card 3",
-      date: "Dec 20 - Jan 5, 2025",
-      budget: "$10,000",
-      travelers: 4,
+    { 
+      tripname: "Sample Card 3",
+      destination: "Delhi",
+      budget: "2000",
+      travellers: "3",
+      startDay: "2025-07-01T00:00:00.000Z",
+      endDay: "2025-07-10T00:00:00.000Z",
+      user: "6792633eaa08eb6efef02261",
+      tripObjId: "679e6ec4189dd60ff447e804"
     },
   ]);
   const router = useRouter();
@@ -37,7 +49,7 @@ export default function Dashboard() {
     <div className=" min-h-screen font-sans" style={{backgroundColor:"var(--accent-1)"}}>
       <main className="max-w-6xl mx-auto p-4 " >
         <div className="flex justify-start items-center my-6">
-        <TripForm buttonValue="+ Create New Trip" title="Create New Trip" actionTitle="Create Trip"/>
+        <TripForm buttonValue="+ Create New Trip" title="Create New Trip" actionTitle="Create Trip" action="new" />
         </div>
 
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
@@ -48,25 +60,25 @@ export default function Dashboard() {
               className=" p-6 rounded shadow flex flex-col justify-between transition-transform duration-300 ease-in-out hover:shadow-lg hover:scale-105"
             >
               <div className="flex justify-between items-center mb-4">
-                <h2 className="text-xl font-bold">{trip.title}</h2>
+                <h2 className="text-xl font-bold">{trip.tripname}</h2>
                 <div className="flex space-x-4">
                 
                   <div >
                   
-                  <TripForm buttonValue={<FaEdit className="text-xl" />} title="Edit Trip" actionTitle="Save Trip"/>
+                  <TripForm buttonValue={<FaEdit className="text-xl" />} title="Edit Trip" actionTitle="Save Trip" action="edit" id= {trip.tripObjId} tripInfo={trip}/>
                   </div>
                   
                   <div>
-                  <DeleteConfirm buttonValue={<FaTrash className="text-xl" />}/>
+                  <DeleteConfirm buttonValue={<FaTrash className="text-xl" />} id={trip.tripObjId}/>
                     
                   </div>
                 </div>
               </div>
 
               <div>
-                <p className="text-gray-600">{trip.date}</p>
+                <p className="text-gray-600">{trip.startDay}</p>
                 <p className="text-gray-600">Budget: {trip.budget}</p>
-                <p className="text-gray-600">Travelers: {trip.travelers}</p>
+                <p className="text-gray-600">Travelers: {trip.travellers}</p>
               </div>
 
               <div className="flex justify-between items-center mt-6">
