fbe8385a6e5af6e6b464f540a60d8986a00ba39f
Suhani Shaikh <139767824+Suhanii-13@users.noreply.github.com>
2025-02-08 10:53:12 +0530

    feat: add view detail page UI (#73)
    * feat: add view detail page
    
    * Improved layout based on review feedback
    added the card for trip info with add destination functionality 
    ---------
    
    Co-authored-by: =Suhanii-13 <=suhanishaikh.1304@gmail.com>

--- wonderlust/src/app/dashboard/page.js
+++ wonderlust/src/app/dashboard/page.js
@@ -7,7 +7,6 @@ import Navbar from "../../components/Navbar";
 import TripForm from "@/src/components/TripForm";
 import DeleteConfirm from "@/src/components/DeleteConfirm";
 
-
 export default function Dashboard() {
   const [trips, setTrips] = useState([
     {
@@ -18,9 +17,9 @@ export default function Dashboard() {
       startDay: "2025-07-01T00:00:00.000Z",
       endDay: "2025-07-10T00:00:00.000Z",
       user: "6792633eaa08eb6efef02261",
-      tripObjId: "679e6e05189dd60ff447e7fd"
+      tripObjId: "679e6e05189dd60ff447e7fd",
     },
-    { 
+    {
       tripname: "Sample Card 2",
       destination: "Delhi",
       budget: "2000",
@@ -28,9 +27,9 @@ export default function Dashboard() {
       startDay: "2025-07-01T00:00:00.000Z",
       endDay: "2025-07-10T00:00:00.000Z",
       user: "6792633eaa08eb6efef02261",
-      tripObjId: "679e6e05189dd60ff447e7fd"
+      tripObjId: "679e6e05189dd60ff447e7ff",
     },
-    { 
+    {
       tripname: "Sample Card 3",
       destination: "Delhi",
       budget: "2000",
@@ -38,39 +37,52 @@ export default function Dashboard() {
       startDay: "2025-07-01T00:00:00.000Z",
       endDay: "2025-07-10T00:00:00.000Z",
       user: "6792633eaa08eb6efef02261",
-      tripObjId: "679e6ec4189dd60ff447e804"
+      tripObjId: "679e6ec4189dd60ff447e804",
     },
   ]);
   const router = useRouter();
 
-  
-
   return (
-    <div className=" min-h-screen font-sans" style={{backgroundColor:"var(--accent-1)"}}>
-      <main className="max-w-6xl mx-auto p-4 " >
+    <div
+      className=" min-h-screen font-sans"
+      style={{ backgroundColor: "var(--accent-1)" }}
+    >
+      <main className="max-w-6xl mx-auto p-4 ">
         <div className="flex justify-start items-center my-6">
-        <TripForm buttonValue="+ Create New Trip" title="Create New Trip" actionTitle="Create Trip" action="new" />
+          <TripForm
+            buttonValue="+ Create New Trip"
+            title="Create New Trip"
+            actionTitle="Create Trip"
+            action="new"
+          />
         </div>
 
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {trips.map((trip, index) => (
             <div
               key={index}
-              style={{backgroundColor:"var(--gray-3)"}}
+              style={{ backgroundColor: "var(--gray-3)" }}
               className=" p-6 rounded shadow flex flex-col justify-between transition-transform duration-300 ease-in-out hover:shadow-lg hover:scale-105"
             >
               <div className="flex justify-between items-center mb-4">
                 <h2 className="text-xl font-bold">{trip.tripname}</h2>
                 <div className="flex space-x-4">
-                
-                  <div >
-                  
-                  <TripForm buttonValue={<FaEdit className="text-xl" />} title="Edit Trip" actionTitle="Save Trip" action="edit" id= {trip.tripObjId} tripInfo={trip}/>
-                  </div>
-                  
                   <div>
-                  <DeleteConfirm buttonValue={<FaTrash className="text-xl" />} id={trip.tripObjId}/>
-                    
+                    <TripForm
+                      buttonValue={<FaEdit className="text-xl" />}
+                      title="Edit Trip"
+                      actionTitle="Save Trip"
+                      action="edit"
+                      id={trip.tripObjId}
+                      tripInfo={trip}
+                    />
+                  </div>
+
+                  <div>
+                    <DeleteConfirm
+                      buttonValue={<FaTrash className="text-xl" />}
+                      id={trip.tripObjId}
+                    />
                   </div>
                 </div>
               </div>
@@ -88,10 +100,8 @@ export default function Dashboard() {
                 >
                   Planning
                 </a>
-                <a
-                  href="#"
-                  className="text-blue-600 font-medium hover:underline hover:text-blue-700 transition-colors duration-300"
-                >
+
+                <a className="text-blue-600 font-medium hover:underline hover:text-blue-700 transition-colors duration-300 cursor-pointer">
                   View Details
                 </a>
               </div>
