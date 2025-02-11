2fcd483443c891c5de7c6adb09c0f05d53f098af
umesh <umesharma.us11@gmail.com>
2025-02-11 20:51:33 +0530

    Fixed the dark theme issue in input field, and flash message on form submission when signed in with google

--- wonderlust/src/app/auth/signin/page.js
+++ wonderlust/src/app/auth/signin/page.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { FaGoogle } from 'react-icons/fa';
 import { handleGoogleLogin } from '@/util/helper';
+import { useSession } from 'next-auth/react';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({ email: '', password: '' });
@@ -11,6 +12,7 @@ export default function SignIn() {
   const [flashMessage, setflashMessage] = useState('');
   const [flashMessageType, setflashMessageType] = useState('');
   const router = useRouter();
+  const { data: session } = useSession();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -64,74 +66,76 @@ export default function SignIn() {
       className="form-container"
       style={{ backgroundColor: 'var(--gray-4)' }}
     >
-      <form
-        onSubmit={handleSubmit}
-        className="form transition-all duration-300 ease-in mb-2 w-[90%] sm:w-[500px] shadow-sm px-6 md:px-20 py-10 rounded-md"
+      <div
+        className="form transition-all duration-300 ease-in mb-2 w-[90%] sm:w-[500px] shadow-sm px-6 md:px-20 md:py-16 py-10 rounded-md"
         style={{ backgroundColor: 'var(--gray-1)' }}
       >
-        <h2 className="md:text-3xl text-2xl text-bold mb-6 text-center font-bold">
-          Login to Wanderlust
-        </h2>
-        {flashMessage && (
-          <div
-            className={`mb-4 p-2 text-center rounded-md 
+        <form onSubmit={handleSubmit} className="w-full">
+          <h2 className="md:text-3xl text-2xl text-bold mb-6 text-center font-bold">
+            Login to Wanderlust
+          </h2>
+          {flashMessage && (
+            <div
+              className={`mb-4 p-2 text-center rounded-md 
             ${
               flashMessageType === 'success'
                 ? 'bg-green-200 text-green-700'
                 : 'bg-red-200 text-red-700'
             }`}
-          >
-            {flashMessage}
+            >
+              {flashMessage}
+            </div>
+          )}
+
+          <div className="flex flex-col gap-2 font-bold">
+            Email
+            <input
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="form-field outline-none border-none font-normal "
+              style={{ backgroundColor: 'var(--gray-1)', color: '--gray-12' }}
+              type="email"
+              placeholder="Email"
+            />
           </div>
-        )}
-
-        <div className="flex flex-col gap-2 font-bold">
-          Email
-          <input
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="form-field outline-none border-none font-normal text-gray-700"
-            type="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="flex flex-col gap-2 font-bold">
-          Password
-          <input
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="form-field outline-none border-none font-normal text-gray-700"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <p className="text-sm my-1">
-          Don't have an account?
-          <Link
-            href="/auth/signup"
-            className=" hover:text-blue-800 font-semibold"
+          <div className="flex flex-col gap-2 font-bold">
+            Password
+            <input
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="form-field outline-none border-none font-normal "
+              style={{ backgroundColor: 'var(--gray-1)', color: '--gray-12' }}
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <p className="text-sm my-1">
+            Don't have an account?
+            <Link
+              href="/auth/signup"
+              className=" hover:text-blue-800 font-semibold"
+            >
+              {' '}
+              Register
+            </Link>
+          </p>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="px-2 w-auto py-2 rounded-lg my-2 bg-gray-100 hover:bg-gray-200 shadow-sm text-gray-800 font-semibold "
           >
-            {' '}
-            Register
-          </Link>
-        </p>
+            Login
+          </button>
+        </form>
         <button
-          type="submit"
-          disabled={isSubmitting}
-          className="px-2 w-auto py-2 rounded-lg my-1 bg-gray-100 hover:bg-gray-200 shadow-sm text-gray-800 font-semibold "
-        >
-          Login
-        </button>
-
-        <button
-          className="px-2 w-auto py-2 rounded-lg my-1 bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold  flex items-center justify-center gap-3"
+          className="px-2 w-full py-2 rounded-lg my-2 bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold  flex items-center justify-center gap-3"
           onClick={() => handleGoogleLogin()}
         >
           <FaGoogle /> Continue with Google
         </button>
-      </form>
+      </div>
     </div>
   );
 }
--- wonderlust/src/app/auth/signup/page.js
+++ wonderlust/src/app/auth/signup/page.js
@@ -67,84 +67,88 @@ export default function SignUp() {
       className="form-container"
       style={{ backgroundColor: 'var(--gray-4)' }}
     >
-      <form
+      <div
+        className="form transition-all duration-300 ease-in mb-2 w-[90%] sm:w-[500px] shadow-sm px-6 md:px-20 md:py-16 py-10 rounded-md"
         style={{ backgroundColor: 'var(--gray-1)' }}
-        onSubmit={handleSubmit}
-        className="form transition-all duration-300 ease-in mb-2 w-[90%] sm:w-[500px] bg-white shadow-sm px-6 md:px-20 py-10 rounded-md"
       >
-        <h2 className="md:text-3xl text-2xl text-bold mb-6 text-center font-bold">
-          Welcome to Wanderlust
-        </h2>
+        <form onSubmit={handleSubmit} className="w-full">
+          <h2 className="md:text-3xl text-2xl text-bold mb-6 text-center font-bold">
+            Welcome to Wanderlust
+          </h2>
 
-        {flashMessage && (
-          <div
-            className={`mb-4 p-2 text-center rounded-md 
+          {flashMessage && (
+            <div
+              className={`mb-4 p-2 text-center rounded-md 
             ${
               flashMessageType === 'success'
                 ? 'bg-green-200 text-green-700'
                 : 'bg-red-200 text-red-700'
             }`}
-          >
-            {flashMessage}
+            >
+              {flashMessage}
+            </div>
+          )}
+          <div className="flex flex-col gap-2 font-bold">
+            Name
+            <input
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="form-field outline-none border-none font-normal "
+              type="text"
+              style={{ backgroundColor: 'var(--gray-1)', color: '--gray-12' }}
+              placeholder="Name"
+            />
           </div>
-        )}
-        <div className="flex flex-col gap-2 font-bold">
-          Name
-          <input
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="form-field outline-none border-none font-normal text-gray-700"
-            type="text"
-            placeholder="Name"
-          />
-        </div>
-        <div className="flex flex-col gap-2 font-bold">
-          Email
-          <input
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="form-field outline-none border-none font-normal text-gray-700"
-            type="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="flex flex-col gap-2 font-bold">
-          Password
-          <input
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="form-field outline-none border-none font-normal text-gray-700"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <p className="text-sm my-1">
-          Already have an account?
-          <Link
-            href="/auth/signin"
-            className="hover:text-blue-800 font-semibold"
+          <div className="flex flex-col gap-2 font-bold">
+            Email
+            <input
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="form-field outline-none border-none font-normal "
+              type="email"
+              style={{ backgroundColor: 'var(--gray-1)', color: '--gray-12' }}
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex flex-col gap-2 font-bold">
+            Password
+            <input
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="form-field outline-none border-none font-normal "
+              style={{ backgroundColor: 'var(--gray-1)', color: '--gray-12' }}
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <p className="text-sm my-1">
+            Already have an account?
+            <Link
+              href="/auth/signin"
+              className="hover:text-blue-800 font-semibold"
+            >
+              {' '}
+              Login
+            </Link>
+          </p>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="px-2 w-auto py-2 rounded-lg my-1 bg-gray-100 hover:bg-gray-200 shadow-sm text-gray-800 font-semibold "
           >
-            {' '}
-            Login
-          </Link>
-        </p>
+            Register
+          </button>
+        </form>
         <button
-          type="submit"
-          disabled={isSubmitting}
-          className="px-2 w-auto py-2 rounded-lg my-1 bg-gray-100 hover:bg-gray-200 shadow-sm text-gray-800 font-semibold "
-        >
-          Register
-        </button>
-        <button
-          className="px-2 w-auto py-2 rounded-lg my-1 bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold  flex items-center justify-center gap-3"
+          className="px-2 w-full py-2 rounded-lg my-2 bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold  flex items-center justify-center gap-3"
           onClick={() => handleGoogleLogin()}
         >
           <FaGoogle /> Continue with Google
         </button>
-      </form>
+      </div>
     </div>
   );
 }
