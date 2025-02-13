1e8751254661f50532b12e841e483a28780890d3
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2025-02-13 19:35:52 +0530

    trip details UI (#87)

--- wonderlust/src/app/dashboard/tripdetails/page.js
+++ wonderlust/src/app/dashboard/tripdetails/page.js
@@ -1,12 +1,18 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Button } from "@radix-ui/themes";
-import Sidebar from "@/src/components/Sidebar"
+import {  TextField} from "@radix-ui/themes";
+import Sidebar from "@/src/components/Sidebar";
+import Accomodation from "@/src/components/TripDetails/Accomodation"
+import Transport from "@/src/components/TripDetails/Transport"
+import Loactions from "@/src/components/TripDetails/Locations"
 import Timeline from "@/src/components/Timeline"
+import SearchResultCard from "@/src/components/SearchResultCard"
 
 export default function TripDetail() {
   const [trip, setTrip] = useState(null);
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+
 
   useEffect(() => {
     const trips = [
@@ -25,11 +31,11 @@ export default function TripDetail() {
     setTrip(trips[0]);
   }, []);
 
- 
+
 
   const events = [
     { title: "Transportation Medium", date: "Jan 10, 2024", description: "Train" },
-    { title: "Accommodation", date: "Jan 12, 2024", description: "hotels" },
+    Accomodation(sidebarOpen, setSidebarOpen),
     { title: "Places to visit", date: "Jan 13, 2024", description: "" },
   ];
 
@@ -77,20 +83,34 @@ export default function TripDetail() {
       
 
        
-      <Button
-        className="p-2 bg-blue-600 text-white rounded-md"
-        onClick={() => setSidebarOpen(true)}
-      >
-        Open Sidebar
-      </Button>
+      
 
       <div className="p-6 shadow-md ">
-          <Timeline  events={events}/>
+   
+            {/* <Accomodation setSidebarOpen={setSidebarOpen}/> */}
+            <Timeline>
+            <Accomodation setSidebarOpen={setSidebarOpen}/>
+            <Transport setSidebarOpen={setSidebarOpen}/> 
+            <Loactions setSidebarOpen={setSidebarOpen}/> 
+
+
+  
+</Timeline>
+
+    
         </div>
     </div>
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)}>
-        <h2 className="text-xl font-bold">Search Bar</h2>
-        <p>Results</p>
+        <TextField.Root placeholder="Search...">
+    <TextField.Slot>
+    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+    </TextField.Slot>
+  </TextField.Root>
+        
+      <SearchResultCard/>
+
+      <SearchResultCard/>
+         
       </Sidebar>
       {/* Timeline div */}
 
---
+++ wonderlust/src/components/TripDetails/Transport.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+import * as Accordion from "@radix-ui/react-accordion";
+import { Button } from "@radix-ui/themes";
+import Timeline from "@/src/components/Timeline"
+
+function Transport({setSidebarOpen}) {
+const [isTransport, setIsTransport] = useState(false);
+const desc = isTransport ? "Transport Medium " : <Button className="p-2 bg-blue-600 text-white rounded-md" onClick={() => setSidebarOpen(true)}>
+        Open Sidebar
+      </Button>
+
+    return ( 
+    <Timeline.Item>
+        <Accordion.Item value="item-1" className="border rounded-lg overflow-hidden">
+          <Timeline.Title>Transportation Medium</Timeline.Title>
+          <Accordion.Content className="p-4 bg-blue-100 text-gray-800">
+            <Timeline.Date>12 Jan 2025</Timeline.Date>
+            <Timeline.Event>{desc}</Timeline.Event>
+          </Accordion.Content>
+        </Accordion.Item>
+    </Timeline.Item>
+      )
+}
+
+export default Transport;
